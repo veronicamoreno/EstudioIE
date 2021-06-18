cf209c72cfb3333443de43dd166fe0d89fde45fe
Veronica Moreno <46247146+veronicamoreno@users.noreply.github.com>
2021-06-18 22:29:10 +0100

    privacidad html/css upload + scroll up button html/css/js + MediaQuery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,9 @@
+
+
+// ----------------------
+// Navigation bar
+// ----------------------
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -50,3 +56,25 @@ window.addEventListener('DOMContentLoaded', event => {
     )
 }
 );
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
